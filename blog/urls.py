bc9035fca73f56b75c19d58aceecbb4fce079091
Lisaveta-K <lisavaeta.khudiakova@gmail.com>
2018-02-16 12:35:08 +0800

    Page for writin new post

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import handler404, handler500
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
 
 handler404 = blog_views.error_404
